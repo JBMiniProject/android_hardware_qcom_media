3abc9c17c4d231578940874d244c339338af2248
Rashed Abdel-Tawab <rashedabdeltawab@gmail.com>
2012-11-16 22:39:01 -0500

    Bring back media for Qualcomm MSM7x27
    Reimplemented a few things dealing with MSM7x27 and added a few
    things that didn't exist at the time when it still had "offical"
    video support
    
    Change-Id: I257972b4dcaf846d5be230a0461f7f15e071e46a

--- mm-video/vidc/venc/Android.mk
+++ mm-video/vidc/venc/Android.mk
@@ -39,6 +39,9 @@ endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7627a)
 libmm-venc-def += -DMAX_RES_720P
 endif
+ifeq ($(TARGET_BOARD_PLATFORM),msm7627)
+libmm-venc-def += -DMAX_RES_720P
+endif
 ifeq ($(TARGET_BOARD_PLATFORM),msm7x30)
 libmm-venc-def += -DMAX_RES_720P
 endif
